82c738dc5b5a9391ea1301bee9a3987d9ae262ae
Abdullah Alturaigi <67443717+Atura7@users.noreply.github.com>
2020-06-30 18:56:52 -0700

    Update UserInterface.java
    updated userinerface

--- CSE360Project/src/UserInterface.java
+++ CSE360Project/src/UserInterface.java
@@ -1,3 +1,4 @@
+//Group 4
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
@@ -16,8 +17,8 @@ public class UserInterface extends Application
       rootPane.getChildren().add(gui);
    
       // Create a scene and place rootPane in the stage
-      Scene scene = new Scene(rootPane, 600, 400);
-      primaryStage.setTitle("Statics Application"); 
+      Scene scene = new Scene(rootPane, 800, 500);
+      primaryStage.setTitle("Stats GUI"); 
       primaryStage.setScene(scene); // Place the scene in the stage
       primaryStage.show(); // Display the stage
    }
