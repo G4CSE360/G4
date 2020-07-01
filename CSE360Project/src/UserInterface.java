a7e386e99ff866f1754ea0ae887e2ef4ec2b4bad
annahuber <annahuber@localhost>
2020-06-30 18:00:21 -0700

    New Changes

--- CSE360Project/src/UserInterface.java
+++ CSE360Project/src/UserInterface.java
@@ -16,7 +16,7 @@ public class UserInterface extends Application
       rootPane.getChildren().add(gui);
    
       // Create a scene and place rootPane in the stage
-      Scene scene = new Scene(rootPane, 800, 800);
+      Scene scene = new Scene(rootPane, 800, 500);
       primaryStage.setTitle("Stats GUI"); 
       primaryStage.setScene(scene); // Place the scene in the stage
       primaryStage.show(); // Display the stage
